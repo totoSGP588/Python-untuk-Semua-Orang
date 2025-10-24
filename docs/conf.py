41316ea836ed15e64942018085bf2bb41ae0ff2f
DavinciPedro <buayabuntung794@gmail.com>
2025-10-25 00:12:08 +0700

    Add Sphinx configuration for documentation

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+# docs/conf.py
+project = 'Python untuk Semua Orang'
+author = 'Kontributor Indonesia'
+release = '1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
